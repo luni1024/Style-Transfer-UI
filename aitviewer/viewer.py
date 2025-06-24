1b84d5ece2fc469d567be78f930e7ccce92ee130
Stefan Bochinger <stefanbochinger@gmail.com>
2025-06-24 22:09:38 +0200

    Use new method in rendering process to retrieve SMPL type nodes

--- aitviewer/viewer.py
+++ aitviewer/viewer.py
@@ -539,13 +539,15 @@ class Viewer(moderngl_window.WindowConfig):
                 self.scene.current_frame_id = (self.scene.current_frame_id + frames) % self.scene.n_frames
                 self._last_frame_rendered_at += frames * (1.0 / self.playback_fps)
 
-        seq_amass = self.scene.get_node_by_uid(1)
-        if self.scene.current_frame_id in seq_amass.keyframes_indices:
-            seq_amass.skeleton_seq.color = (0, 255, 0 , 1)
-            seq_amass.mesh_seq.color = (0, 255, 0 , 0.5)
-        else:
-            seq_amass.skeleton_seq.color = seq_amass.skeleton_default_color
-            seq_amass.mesh_seq.color = seq_amass.mesh_default_color
+        # SMPL type nodes have highlightable keyframes
+        seq_smpl = self.scene.get_node_by_attribute("smpl_layer")
+        if seq_smpl != None:
+            if self.scene.current_frame_id in seq_smpl.keyframes_indices:
+                seq_smpl.skeleton_seq.color = (0, 255, 0 , 1)
+                seq_smpl.mesh_seq.color = (0, 255, 0 , 0.5)
+            else:
+                seq_smpl.skeleton_seq.color = seq_smpl.skeleton_default_color
+                seq_smpl.mesh_seq.color = seq_smpl.mesh_seq.default_color
 
         # Update camera matrices that will be used for rendering
         if isinstance(self.scene.camera, ViewerCamera):
